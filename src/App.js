9ecd5988809bff6cd3f5b9bf16c6a577f6760814
justrush <justinrushwa@gmail.com>
2021-04-13 08:42:05 -0700

    modified app html to refelct confirmation note

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          This confirms the frontend data pipeline is sound.
+        </p>
         <a
           className="App-link"
           href="https://amazon.com"
