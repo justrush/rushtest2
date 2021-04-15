ef489351b194358b6b8fb4219f183b15482b38f6
justrush <justinrushwa@gmail.com>
2021-04-15 13:08:59 -0700

    Changes to html

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          This confirms the frontend data pipeline is sound.
+          CoreUI wil be here.
         </p>
         <a
           className="App-link"
